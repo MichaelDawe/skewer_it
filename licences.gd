2b5ff6b78e74a508b98b9a5250956d148dc14806
MikeRedwood <michaeldawe012@gmail.com>
2023-05-08 15:33:54 +0100

    updated licences

--- licences.gd
+++ licences.gd
@@ -22,6 +22,19 @@ func _on_next_pressed():
 		0:
 			index += 1
 			$VBoxContainer/MarginContainer/licencetext.set_text(
+				'This game uses Godot Engine, available under the following license:
+
+Copyright (c) 2014-present Godot Engine contributors. Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
+)
+		1:
+			index += 1
+			$VBoxContainer/MarginContainer/licencetext.set_text(
 				'Godot includes ENet, available under the following license:
 
 Copyright (c) 2002-2020 Lee Salzman
@@ -32,7 +45,7 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
 )
-		1:
+		2:
 			index += 1
 			$VBoxContainer/MarginContainer/licencetext.set_text(
 				'Godot includes mbed TLS, available under the following license:
@@ -45,14 +58,14 @@ http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.'
 )
-		2:
+		3:
 			index += 1
 			$VBoxContainer/MarginContainer/licencetext.set_text(
 				'Godot uses FreeType to render fonts, available under the following license:
 
 Portions of this software are copyright © 2023 The FreeType Project (www.freetype.org). All rights reserved.'
 )
-		3:
+		4:
 			var credits = preload("res://credits.tscn").instantiate()
 			get_node("/root/main").add_child(credits)
 			queue_free()
